f82c2a1d92d3ec302520607dd7625c5b94d12d6a
Blue-Beaker <yyy5566778899@126.com>
2025-02-21 23:31:35 +0800

    add thresold and crop options

--- cut.py
+++ cut.py
@@ -9,29 +9,48 @@ import numpy as np
 
 os.chdir(sys.path[0])
 
-SHOW=False #是否要在处理时显示帧. 很慢!
-#Whether to show image when processing. VERY SLOW!
+SHOW=False #是否要在处理时显示帧. 会变慢!
+#Whether to show image when processing. Slows down!
 
 COLOR_THRESOLD=10 #颜色判定阈值. 0~255,0=黑,255=白
 #Thresold to determine black color. 0~255, black = 0, white = 255
 
+PERCENTAGE_1=99 #黑色占所选区域百分比大于此值判定开始黑屏
+PERCENTAGE_2=98 #黑色占所选区域百分比小于此值判定结束黑屏
+
+# 在此设置需要判定黑屏的范围
+x1=0
+x2=1600
+y1=0
+y2=900
+
+def crop(frame:np.ndarray):
+    return frame[y1:y2,x1:x2]
+    # return frame # 或无需裁剪
+
+
+thr1=255-(PERCENTAGE_1*255/100)
+thr2=255-(PERCENTAGE_2*255/100)
 
 class VideoCutter:
     def process(self):
-        while (self.cap.isOpened()):
+        while ((not self.finished) and self.cap.isOpened()):
             # 由于已经二值化,此平均值反映的是非黑色部分占总画面的比例. 
             mean=self.process_frame()
 
             if(self.last_frame_black):
-                if mean>2: # 黑屏结束判定
+                if mean>thr2: # 黑屏结束判定
                     self.last_frame_black=False
                     self.start_subtitle()
             else:
-                if mean<1: # 黑屏开始判定
+                if mean<thr1: # 黑屏开始判定
                     self.last_frame_black=True
                     self.end_subtitle()
 
             self.frameIndex=self.frameIndex+1
+
+            print("\r"+f"frames={self.frameIndex} mean={mean}",end="")
+
     def start_subtitle(self):
         self.last_subtitle_start_frame=self.frameIndex
 
@@ -57,8 +76,13 @@ class VideoCutter:
         # Loop until the end of the video
         # Capture frame-by-frame
         ret, frame = self.cap.read()
+
         if(not ret):
+            self.finished=True
             return 255.0
+        
+        frame=crop(frame)
+        
         frame = cv2.resize(frame, (320,180), fx = 0, fy = 0,
                             interpolation = cv2.INTER_LINEAR)
 
@@ -83,6 +107,7 @@ class VideoCutter:
         self.subtitle_file=open(videoname+".srt","w")
         self.subtitle_line_id:int=1
         self.last_subtitle_start_frame:int=0
+        self.finished=False
 
         info=pymediainfo.MediaInfo.parse(videoname).video_tracks[0] # type: ignore
         data=info.to_data()
@@ -106,6 +131,7 @@ if(sys.argv.__len__()<2):
     print("Usage: cut.py [VIDEO FILE]")
     exit()
 try:
+    print(f"Thresold: {thr1,thr2}")
     cutter = VideoCutter(sys.argv[1])
     cutter.process()
 finally:
